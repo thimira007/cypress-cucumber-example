44dedf48b649fd0f0ba8a64d30f022f283c6265d
Thimira Navarathna <41164898+thimira007@users.noreply.github.com>
2022-07-27 19:49:19 +0530

    Update cucumber-html-report.js

--- cypress/reports/cucumber-html-report.js
+++ cypress/reports/cucumber-html-report.js
@@ -1,7 +1,7 @@
 const report = require('multiple-cucumber-html-reporter');
 report.generate({
-    jsonDir: './cypress/cucumber-json',  // ** Path of .json file **//
-    reportPath: './cypress/html-report',
+    jsonDir: './cucumber-json',  // ** Path of .json file **//
+    reportPath: './html-report',
     metadata: {
         environment: 'STAGE',
         platform: {
